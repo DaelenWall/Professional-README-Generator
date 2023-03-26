33e9897a5dc2a09ed2762a7f464ab135252c9dac
Daelen Wall <daelenwall1@gmail.com>
2023-03-25 21:25:52 -0400

    adding inquirer prompts + beginning of const generateREADME.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,79 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your repository?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'enter the description for your repository.',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter the installation instructions for your repository.',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter the usage information for your repository.',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter the contribution guidelines for your repository.',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter the test instructions for your repository.',
 
-// TODO: Create a function to initialize app
-function init() {}
+      type: 'list',
+      name: 'license',
+      message: 'Which license would you like to use?',
+      choices: 'Apache, MIT, BSD, Unlicense',
 
-// Function call to initialize app
-init();
+      type: 'input',
+      name: 'github',
+      message: 'Please enter your Github username.',
+
+      type: 'input',
+      name: 'email',
+      message: 'Please enter your Email address.',
+    },
+  ]);
+};
+
+const generateREADME = ({ title, description, installation, usage, contributing, tests, license, github, email }) =>
+  `# Title
+    ${title}
+
+  ## Description
+    ${description}
+
+  ### Installation Instructions
+    ${installation}
+
+  #### Usage Information
+    ${usage}
+
+  ##### Contribution Guidelines
+    ${contributing}
+
+  ###### Test Instructions
+    ${tests}
+
+  ####### License
+  License: ${license}
+
+  ######## Questions
+  Github: ${github}
+  Email: ${email}`;
+
+    // add const init to write readme n stuff + promptUser function -- cant forget that
